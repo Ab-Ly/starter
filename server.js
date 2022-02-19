2ce5fa93b17aad131c967f443181a461f99b5cf3
Ab-Ly <abdelalilyoussefi@icloud.com>
2022-02-19 16:55:20 +0100

    Create a tourModel SEPERATE

--- server.js
+++ server.js
@@ -19,37 +19,6 @@ mongoose
   .then(() => console.log('DB connection successful !'));
 
 // MONGOOSE INITIALIZATION 1) Schema
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Park Camper',
-  price: 997,
-});
-
-testTour
-  .save()
-  .then((doc) => {
-    console.log(doc);
-  })
-  .catch((err) => {
-    console.log('ERROR:', err);
-  });
 
 // START SERVER
 const port = process.env.PORT || 3000;
