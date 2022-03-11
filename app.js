4d5063feb1bef8257c85f72cf7c5fc8d659864ce
Ab-Ly <abdelalilyoussefi@icloud.com>
2022-03-11 13:25:14 +0100

    Implementing rate Limiting to Stop lot of Request

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
-// MIDDLEWARES
+// GLOBAL MIDDLEWARES
+
 if (process.env.NODE_ENV !== 'development') {
   app.use(morgan('dev'));
 }
-
+// limit for 100 request and time limit for windows 1 hour for resent IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 60 * 1000,
+  message: 'Too many request from this IP, Please try again in an hour',
+});
+app.use('/api', limiter);
 app.use(express.json());
 
 app.use(express.static(`${__dirname}/public`));
