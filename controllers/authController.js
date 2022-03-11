66ed34f485d2e7b6dcfe1ade23d1f377baf0c6f8
Ab-Ly <abdelalilyoussefi@icloud.com>
2022-03-11 13:02:01 +0100

    Sending Token via Cokies

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,20 +13,31 @@ const signToken = (id) =>
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  // send a cookie
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
 
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
+  // Remove the password from the Output
+  user.password = undefined;
   res
     .status(statusCode)
     .json({ status: 'success', token, data: { user: user } });
 };
 
 exports.signup = catchAsync(async (req, res, next) => {
-  // const newUser = await User.create(req.body);
-  const newUser = await User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-    passwordConfirm: req.body.passwordConfirm,
-  });
+  const newUser = await User.create(req.body);
+  // const newUser = await User.create({
+  //   name: req.body.name,
+  //   email: req.body.email,
+  //   password: req.body.password,
+  //   passwordConfirm: req.body.passwordConfirm,
+  // });
   createSendToken(newUser, 201, res);
 });
 
