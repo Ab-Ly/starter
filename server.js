c7fc76d0a95b17674c83c0bf2342628e890ca8a3
Ab-Ly <96085607+Ab-Ly@users.noreply.github.com>
2022-03-20 22:48:01 +0100

    Added Heroku config

--- server.js
+++ server.js
@@ -48,3 +48,10 @@ process.on('uncaughtException', (err) => {
     process.exit(1);
   });
 });
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM RECEIVED. Shutting down gracefully');
+  server.close(() => {
+    console.log('Process Terminated');
+  });
+});
